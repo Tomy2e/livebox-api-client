6002b8a3e1b35a92237d0ac6677cd8e3dd755034
Tomy Guichard <tomy2e@live.fr>
2023-03-02 22:25:14 +0100

    Make the client thread-safe

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package livebox provides a client to easily communicate with Livebox 5's API.
+//
+// This API is usually available at `http://192.168.1.1/ws`. Authentication is
+// handled by the library, set the `admin` password and start sending requests.
+package livebox

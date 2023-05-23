0d98b258e4faadf5f593ec7c0d60fc12f42e143d
Tomy Guichard <tomy2e@live.fr>
2023-05-23 20:43:26 +0200

    Add client options

--- client.go
+++ client.go
@@ -1,43 +1,17 @@
 package livebox
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
 	"net/http"
 
-	"github.com/Tomy2e/livebox-api-client/api/request"
-	"github.com/Tomy2e/livebox-api-client/api/response"
 	internalHTTP "github.com/Tomy2e/livebox-api-client/internal/http"
 )
 
+const defaultLiveboxAddress = "http://192.168.1.1"
+
 // Client is a Livebox API Client. Requests sent using a client will
-// be automatically authenticated. Client is thread safe.
-type Client interface {
-	Request(ctx context.Context, req *request.Request, out interface{}) error
-}
-
-// NewClient returns a new Client that will be authenticated using the given
-// password.
-func NewClient(password string) Client {
-	return NewClientWithHTTPClient(password, &http.Client{})
-}
-
-// NewClientWithHTTPClient returns a new Client that will be authenticated
-// using the given password. The given HTTP client will be used to send
-// HTTP requests.
-func NewClientWithHTTPClient(password string, c *http.Client) Client {
-	httpClient := internalHTTP.NewClient(c)
-
-	return &client{
-		password: password,
-		client:   httpClient,
-		session:  newSession(httpClient),
-	}
-}
-
-// client implements the Client interface.
-type client struct {
+// be automatically authenticated using the specified password.
+// Client is thread safe.
+type Client struct {
 	// HTTP client that will be used to send HTTP requests.
 	client *internalHTTP.Client
 	// Password of the "admin" user.
@@ -46,64 +20,62 @@ type client struct {
 	session *session
 }
 
-// Request sends a request to the Livebox API. If the client is not yet
-// authenticated, or the session is expired, the client will try to
-// authenticate using the admin password given during the creation
-// of the client.
-func (c *client) Request(ctx context.Context, req *request.Request, out interface{}) error {
-	// Try to authenticate if it's the first request.
-	if _, err := c.session.Renew(ctx, c.password, renewIfNotInitialized); err != nil {
-		return err
+// NewClient returns a new Client that will be authenticated using the given
+// password.
+func NewClient(password string, opts ...Opt) (*Client, error) {
+	var (
+		err error
+		co  = newClientOpts(opts)
+		c   = &Client{
+			password: password,
+			client:   internalHTTP.NewClient(co.httpClient),
+		}
+	)
+
+	c.session, err = newSession(c.client, co.address)
+	if err != nil {
+		return nil, err
 	}
 
-	// Send the request, if the session is expired, the client will renew the session.
-	return c.request(ctx, req, out)
+	return c, nil
 }
 
-// request sends a request to the Livebox API. Before calling this function,
-// the client must have been authenticated successfully in the past.
-// This function handles reauthentication when the session expires.
-// Reauthentication will only be attempted once.
-func (c *client) request(ctx context.Context, req *request.Request, out interface{}) error {
-	// Create request payload
-	payload, err := json.Marshal(req)
-	if err != nil {
-		return err
+// clientOpts contain client custom options.
+type clientOpts struct {
+	address    string
+	httpClient *http.Client
+}
+
+// newClientOpts returns a clientOpts object with the custom options.
+// If an option was not specified, the default value for this option is used.
+func newClientOpts(opts []Opt) *clientOpts {
+	co := &clientOpts{
+		httpClient: http.DefaultClient,
+		address:    defaultLiveboxAddress,
 	}
 
-	authAttempted := false
-
-	for {
-		// Create HTTP request with request payload
-		r, v, err := c.session.NewAuthenticatedRequest(ctx, bytes.NewReader(payload))
-		if err != nil {
-			return err
-		}
-
-		if _, err := c.client.SendRequest(ctx, r, out); err != nil {
-			// If reauthentication was already attempted, return error now.
-			if authAttempted {
-				return err
-			}
-
-			// Check if the server returned a permission denied error.
-			if response.IsPermissionDeniedError(err) {
-				// Try to renew the session if the version of the session that
-				// was used is still the current one.
-				if _, err := c.session.Renew(ctx, c.password, renewIfVersionIsCurrent(v)); err != nil {
-					return err
-				}
-
-				// Successful reauthentication. Retry request one more time.
-				authAttempted = true
-				continue
-			}
-
-			return err
-		}
-
-		break
+	for _, f := range opts {
+		f(co)
 	}
 
-	return nil
+	return co
+}
+
+// Opt is a Livebox client option.
+type Opt func(c *clientOpts)
+
+// WithHTTPClient allows using a custom http client. If not used, the Go default
+// HTTP client is used.
+func WithHTTPClient(httpClient *http.Client) Opt {
+	return func(c *clientOpts) {
+		c.httpClient = httpClient
+	}
+}
+
+// WithAddress allows using a custom Livebox address. If not used, the Livebox
+// address is set to http://192.168.1.1.
+func WithAddress(address string) Opt {
+	return func(c *clientOpts) {
+		c.address = address
+	}
 }
